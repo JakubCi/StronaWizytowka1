1d2d0e0b11c175e84a478e1b93d5ab1c14add1b4
JakubCi <90244126+JakubCi@users.noreply.github.com>
2023-01-25 18:49:47 +0100

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -14,10 +14,15 @@ scrollTo = (element) => {
   document.getElementById("menu-items-omnie").addEventListener('click', () => {
     scrollTo(document.querySelector('.experience'));
   });
+  document.getElementById("leftodnosnik").addEventListener('click', () => {
+    scrollTo(document.querySelector('.experience'));
+  });
   document.getElementById("menu-items-doswiadczenie").addEventListener('click', () => {
     scrollTo(document.querySelector('.courses'));
   });
-
+  document.getElementById("menu-items-kontakt").addEventListener('click', () => {
+    scrollTo(document.querySelector('.contact'));
+  });
 var btn = $('#button');
 
 $(window).scroll(function() {
@@ -32,3 +37,4 @@ btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '300');
 });
+
