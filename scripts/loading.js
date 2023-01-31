c568284ad492dc2ff4ec98a943412c005287dec2
JakubCi <90244126+JakubCi@users.noreply.github.com>
2023-01-31 19:47:16 +0100

    Add files via upload

---
+++ scripts/loading.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded',()=>{
+    document.querySelector('.loader').classList.add('loader--hidden')
+})
+window.addEventListener('load',()=>{
+    document.querySelector('.loader').classList.add('loader--hidden')
+})
