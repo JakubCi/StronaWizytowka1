4abea3235c2e1bf9228c97b1bc4ad306e423fe42
JakubCi <90244126+JakubCi@users.noreply.github.com>
2023-01-31 19:59:48 +0100

    Update loading.js

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,6 +1,3 @@
-window.addEventListener('DOMContentLoaded',()=>{
-    document.querySelector('.loader').classList.add('loader--hidden')
-})
 window.addEventListener('load',()=>{
     document.querySelector('.loader').classList.add('loader--hidden')
 })
