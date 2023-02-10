8555cc10333af785c38ce44aca618e4db388b852
JakubCi <90244126+JakubCi@users.noreply.github.com>
2023-02-10 12:16:53 +0100

    Update loading.js

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,6 +1,16 @@
 window.addEventListener('load',()=>{
+    
     document.querySelector('.loader').classList.add('loader--hidden')
+    document.querySelector('.loader').classList.remove('loader--show')
+    document.querySelector('.load1').classList.remove('loadinganimation')
+    document.querySelector('.load2').classList.remove('loadinganimation')
+    document.querySelector('.load3').classList.remove('loadinganimation')
 })
 window.addEventListener('DOMContentLoaded',()=>{
+    document.querySelector('.load1').classList.add('loadinganimation')
+    document.querySelector('.load2').classList.add('loadinganimation')
+    document.querySelector('.load3').classList.add('loadinganimation')
     document.querySelector('.loader').classList.add('loader--show')
+    document.querySelector('.loader').classList.remove('loader--hidden')
+    
 })
