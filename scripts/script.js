02b9153e0f579965a13adb95e2f5debfb58161ce
JakubCi <90244126+JakubCi@users.noreply.github.com>
2023-03-04 18:05:31 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -47,11 +47,10 @@ var burger = $(".checkbox");
 $('.menu-items li a').on("click", function (e) {
     burger.click();
   });
-document.addEventListener("resize",()=>{
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    window.addEventListener('resize', () => {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-})
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
