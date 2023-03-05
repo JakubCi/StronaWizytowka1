16fc12d19af030cb3218047a4c6838b63a6534b3
JakubCi <90244126+JakubCi@users.noreply.github.com>
2023-03-05 10:23:16 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -48,8 +48,6 @@ $('.menu-items li a').on("click", function (e) {
     burger.click();
   });
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
